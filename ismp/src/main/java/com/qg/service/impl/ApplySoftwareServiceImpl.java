6b8fa77cb0bcac0e8dcfe829e90bec136f122ca2
huanggengbiao <2864201368@qq.com>
2025-07-21 22:46:46 +0800

    code:申请表部分功能

--- ismp/src/main/java/com/qg/service/impl/ApplySoftwareServiceImpl.java
+++ ismp/src/main/java/com/qg/service/impl/ApplySoftwareServiceImpl.java
@@ -1,8 +1,64 @@
 package com.qg.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.qg.domain.ApplyDeveloper;
+import com.qg.domain.ApplySoftware;
+import com.qg.mapper.ApplySoftwareMapper;
 import com.qg.service.ApplySoftwareService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.qg.util.Constants.*;
 
 @Service
 public class ApplySoftwareServiceImpl implements ApplySoftwareService {
+    @Autowired
+    private ApplySoftwareMapper applySoftwareMapper;
+
+    @Override
+    public List<ApplySoftware> selectAllOrderByTime() {
+        // 创建 Lambda 查询包装器
+        LambdaQueryWrapper<ApplySoftware> wrapper = new LambdaQueryWrapper<>();
+        // 按创建时间降序排序（DESC）
+        wrapper.orderByDesc(ApplySoftware::getApplyTime)
+                .eq(ApplySoftware::getIsDeleted, IS_NOT_DELETED);
+        // 执行查询
+        return applySoftwareMapper.selectList(wrapper);
+    }
+
+    @Override
+    public boolean add(ApplySoftware ApplySoftware) {
+        return applySoftwareMapper.insert(ApplySoftware) > 0;
+    }
+
+    @Override
+    public List<ApplySoftware> selectByUserId(Long userId) {
+        // 创建 Lambda 查询包装器
+        LambdaQueryWrapper<ApplySoftware> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(ApplySoftware::getUserId, userId);
+        return applySoftwareMapper.selectList(wrapper);
+    }
+
+    @Override
+    public boolean delete(ApplySoftware applySoftware) {
+        return applySoftwareMapper.deleteById(applySoftware.getId()) > 0;
+    }
+
+    @Override
+    public ApplySoftware selectById(Long id) {
+        return applySoftwareMapper.selectById(id);
+    }
+
+    @Override
+    public boolean updateStatus(ApplySoftware applySoftware) {
+        if (applySoftware.getStatus() == 0) {
+            applySoftware.setStatus(IS_HANDLED);
+        } else {
+            applySoftware.setStatus(IS_NOT_HANDLED);
+        }
+        applySoftwareMapper.updateById(applySoftware);
+        return false;
+    }
 }
