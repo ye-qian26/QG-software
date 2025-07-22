66b4c73ca35368cf165f55a99d7e60b9762ec4fa
huanggengbiao <2864201368@qq.com>
2025-07-22 14:04:12 +0800

    feat:冻结表功能完成

--- ismp/src/main/java/com/qg/service/BanService.java
+++ ismp/src/main/java/com/qg/service/BanService.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface BanService {
     List<Ban> selectAll();
+
+    boolean add(Ban ban);
+
+    boolean delete(Ban ban);
+
+    Ban selectByUserId(Long userId);
+
+    boolean judgeBan(Long userId);
 }
