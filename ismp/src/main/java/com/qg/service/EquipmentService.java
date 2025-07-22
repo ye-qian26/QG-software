86c01ea8bd0662f283012a1d3a093cbaf143eef8
tzdphxx <17811466341@163.com>
2025-07-22 15:01:56 +0800

    feat(equipment):可以查看是否已经购买了，可以预约和查看预约

--- ismp/src/main/java/com/qg/service/EquipmentService.java
+++ ismp/src/main/java/com/qg/service/EquipmentService.java
@@ -2,6 +2,18 @@ package com.qg.service;
 
 import com.qg.domain.Equipment;
 
+import java.util.List;
+
 public interface EquipmentService {
     int saveEquipment(Equipment equipment);
+
+    List selectPurchased(Long userId);
+
+    boolean isPurchased(Long userId, Long softwareId);
+
+    List<Equipment> selectAppointment(Long userId);
+
+    boolean isAppointment(Long userId, Long softwareId);
+
+    int addAppointment(Equipment equipment);
 }
