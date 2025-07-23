9ea3b66fc4f65bcd25f469364ec3a07a10a14c52
huanggengbiao <2864201368@qq.com>
2025-07-23 20:39:08 +0800

    refactor:拉取代码

--- ismp/src/main/java/com/qg/config/WebConfig.java
+++ ismp/src/main/java/com/qg/config/WebConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
+//    @Autowired
     private TokenInterceptor tokenInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
