1370f96b880d8073f7efb5d9580f5b289e093dad
罗烨烨 <1452756143@qq.com>
2025-07-24 17:17:40 +0800

    refactor(equipment):已购买、已预约、管理员查看已预约的软件分页查询

--- ismp/src/main/java/com/qg/mapper/SoftwareMapper.java
+++ ismp/src/main/java/com/qg/mapper/SoftwareMapper.java
@@ -1,6 +1,8 @@
 package com.qg.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.qg.domain.Software;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -16,10 +18,10 @@ public interface SoftwareMapper extends BaseMapper<Software> {
     List<Software> selectTop10ByStatusOrderByIdDesc(@Param("status") int status);
 
     @Select("SELECT * FROM software WHERE status != #{status} AND type = #{type} ORDER BY id DESC LIMIT 10")
-    List<Software> selectTop10ByStatusAndTypeOrderByIdDesc(@Param("status") int status,@Param("type") String type);
+    List<Software> selectTop10ByStatusAndTypeOrderByIdDesc(@Param("status") int status, @Param("type") String type);
 
     @Select("SELECT * FROM software WHERE status != #{status} AND type = #{type} ORDER BY id DESC ")
-    List<Software> selectByStatusAndTypeOrderByIdDesc(@Param("status") int status,@Param("type") String type);
+    List<Software> selectByStatusAndTypeOrderByIdDesc(@Param("status") int status, @Param("type") String type);
 
     @Update("UPDATE software SET status = #{status} WHERE id = #{id}")
     int updateStatus(@Param("id") Long id, @Param("status") int status);
@@ -29,35 +31,45 @@ public interface SoftwareMapper extends BaseMapper<Software> {
 
     /**
      * 获取用户的所有已预约软件
+     *
      * @param userId
      * @return
      */
     @Select("SELECT s.* FROM software s " +
             "JOIN equipment e ON s.id = e.software_id " +
-            "WHERE e.user_id = #{userId} AND e.status = 0")
-    List<Software> getAllOrderSoftware(@Param("userId") Long userId);
+            "WHERE e.user_id = #{userId} AND e.status = 0 " +
+            "GROUP BY e.id " +
+            "ORDER BY published_time")
+    IPage<Software> getAllOrderSoftware(Page<Software> page, @Param("userId") Long userId);
 
     /**
      * 获取用户的所有已购买软件
+     *
      * @param userId
      * @return
      */
     @Select("SELECT s.* FROM software s " +
             "JOIN equipment e ON s.id = e.software_id " +
-            "WHERE e.user_id = #{userId} AND e.status = 1")
-    List<Software> getAllBuySoftware(@Param("userId") Long userId);
+            "WHERE e.user_id = #{userId} AND e.status = 1 " +
+            "GROUP BY e.id " +
+            "ORDER BY published_time")
+    IPage<Software> selectPurchased(Page<Software> page, @Param("userId") Long userId);
 
     /**
      * 管理员查看所有用户的预约软件
+     *
      * @return
      */
     @Select("SELECT s.* FROM software s " +
             "JOIN equipment e ON s.id = e.software_id " +
-            "WHERE e.status = 0")
-    List<Software> adminGetAllOrderSoftware();
+            "WHERE e.status = 0 " +
+            "GROUP BY e.id " +
+            "ORDER BY published_time")
+    IPage<Software> adminGetAllOrderSoftware(Page<Software> page);
 
     /**
      * 根据软件名称模糊匹配
+     *
      * @param name
      * @return
      */
