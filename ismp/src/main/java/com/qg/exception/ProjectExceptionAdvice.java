32646be47645facd5452ad98406332fd86acbf25
罗烨烨 <1452756143@qq.com>
2025-07-25 08:34:14 +0800

    style(exception):后台捕获异常控制台输出

--- ismp/src/main/java/com/qg/exception/ProjectExceptionAdvice.java
+++ ismp/src/main/java/com/qg/exception/ProjectExceptionAdvice.java
@@ -11,9 +11,6 @@ public class ProjectExceptionAdvice {
     @ExceptionHandler(SystemException.class)
     public Result doSystemException(SystemException ex) {
         System.out.println("==>\n系统异常:\n" + ex.getMessage() + "\n<==");
-        //记录日志
-        //发送消息给运维
-        //发送邮件给开发人员,ex对象发送给开发人员
         return new Result(ex.getCode(), ex.getMessage());
     }
 
@@ -25,9 +22,6 @@ public class ProjectExceptionAdvice {
 
     @ExceptionHandler(Exception.class)
     public Result doOtherException(Exception ex) {
-        //记录日志
-        //发送消息给运维
-        //发送邮件给开发人员,ex对象发送给开发人员
         System.out.println("==>\n未知异常:\n" + ex.getMessage() + "\n<==");
         return new Result(Code.INTERNAL_ERROR, "系统繁忙，请稍后再试！");
     }
