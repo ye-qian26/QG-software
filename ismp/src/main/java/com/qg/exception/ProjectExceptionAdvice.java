5deebb639e81806e1d284682a411b1b912232a0c
罗烨烨 <1452756143@qq.com>
2025-07-25 08:33:30 +0800

    style(exception):后台捕获异常控制台输出

--- ismp/src/main/java/com/qg/exception/ProjectExceptionAdvice.java
+++ ismp/src/main/java/com/qg/exception/ProjectExceptionAdvice.java
@@ -10,7 +10,7 @@ public class ProjectExceptionAdvice {
 
     @ExceptionHandler(SystemException.class)
     public Result doSystemException(SystemException ex) {
-        System.out.println("==>/n系统异常:/n" + ex.getMessage() + "/n<==");
+        System.out.println("==>\n系统异常:\n" + ex.getMessage() + "\n<==");
         //记录日志
         //发送消息给运维
         //发送邮件给开发人员,ex对象发送给开发人员
@@ -19,7 +19,7 @@ public class ProjectExceptionAdvice {
 
     @ExceptionHandler(BusinessException.class)
     public Result doBusinessException(BusinessException ex) {
-        System.out.println("==>/n业务异常:/n" + ex.getMessage() + "/n<==");
+        System.out.println("==>\n业务异常:\n" + ex.getMessage() + "\n<==");
         return new Result(ex.getCode(), ex.getMessage());
     }
 
@@ -28,7 +28,7 @@ public class ProjectExceptionAdvice {
         //记录日志
         //发送消息给运维
         //发送邮件给开发人员,ex对象发送给开发人员
-        System.out.println("==>/n未知异常:/n" + ex.getMessage() + "/n<==");
+        System.out.println("==>\n未知异常:\n" + ex.getMessage() + "\n<==");
         return new Result(Code.INTERNAL_ERROR, "系统繁忙，请稍后再试！");
     }
 
